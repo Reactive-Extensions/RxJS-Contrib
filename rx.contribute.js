bc4d12bf3fc063110edeb5b4538153ad7d7e9dca
Eero Anttila <eero.anttila@reaktor.fi>
2013-08-23 15:43:29 +0300

    Convert documentation to standard format

--- rx.contribute.js
+++ rx.contribute.js
@@ -17,33 +17,36 @@
         observableProto = Observable.prototype,
         isFunction = function(fun) { return typeof(fun) === "function"; };
 
-    /// <summary>
-    /// Filters the observable sequence by the value of itself or a given property on it and lets only those pass where the value is true
-    /// </summary>
-    /// <param name="propertyName">[Optional] Name of the property to check for true.</param>
-    /// <returns>An observable sequence that is filtered either by the value of itself or the value of the given property on itself.</returns>
+    /**
+     * Filters the observable sequence by the value of itself or a given property on it and lets only those pass where the value is true
+     *
+     * @param {String} propertyName [Optional] Name of the property to check for true.
+     * @returns {Observable} An observable sequence that is filtered either by the value of itself or the value of the given property on itself.
+     */
     observableProto.whereTrue = function (propertyName) {
         return this.where(function (x) {
             return propertyName === undefined ? x === true : x[propertyName] === true;
         });
     };
 
-    /// <summary>
-    /// Filters the observable sequence by the value of itself or a given property on it and lets only those pass where the value is false
-    /// </summary>
-    /// <param name="propertyName">[Optional] Name of the property to check for false.</param>
-    /// <returns>An observable sequence that is filtered either by the value of itself or the value of the given property on itself.</returns>
+    /**
+     * Filters the observable sequence by the value of itself or a given property on it and lets only those pass where the value is false
+     *
+     * @param {String} propertyName [Optional] Name of the property to check for false.
+     * @returns {Observable} An observable sequence that is filtered either by the value of itself or the value of the given property on itself.
+     */
     observableProto.whereFalse = function (propertyName) {
         return this.where(function (x) {
             return propertyName === undefined ? x === false : x[propertyName] === false;
         });
     };
 
-    /// <summary>
-    /// projects the current value into a new object that carries the current value on a property with the given name
-    /// </summary>
-    /// <param name="propertyName">Name of the new property</param>
-    /// <returns>An observable sequence with objects that that carry the current value on the specified property</returns>
+    /**
+     * Projects the current value into a new object that carries the current value on a property with the given name
+     *
+     * @param {String} propertyName Name of the new property
+     * @returns {Observable} An observable sequence with objects that that carry the current value on the specified property
+     */
     observableProto.wrapAs = function (propertyName) {
         return this.select(function (x) {
             var temp = {};
@@ -52,12 +55,13 @@
         });
     };
 
-    /// <summary>
-    /// Appends a value or the resulting value of a given function to the current stream object under a given name
-    /// </summary>
-    /// <param name="propertyName">Name of the property to place the data on</param>
-    /// <param name="data">Either the data itself or a function to lazily retrieve the data</param>
-    /// <returns>An observable sequence with objects that carry some injected data on the specified property</returns>
+    /**
+     * Appends a value or the resulting value of a given function to the current stream object under a given name
+     *
+     * @param {String} propertyName Name of the property to place the data on
+     * @param data Either the data itself or a function to lazily retrieve the data
+     * @returns {Observable} An observable sequence with objects that carry some injected data on the specified property
+     */
     observableProto.appendAs = function (propertyName, data) {
         return this.select(function (x) {
             if (x !== null && typeof (x) == 'object') {
@@ -72,13 +76,14 @@
         });
     };
 
-    /// <summary>
-    /// Applies a function on a property of the current value and saves the return value as an property under the same or a new name
-    /// </summary>
-    /// <param name="propertyFrom">Name of the property to pick the data from</param>
-    /// <param name="propertyTo">Name of the property to store the data on</param>
-    /// <param name="transistorFunc">Function to be applied on the value of the property</param>
-    /// <returns>An observable sequence with objects that carry an (possibly) new property with a value coming from a (possibly) different property</returns>
+    /**
+     * Applies a function on a property of the current value and saves the return value as an property under the same or a new name
+     *
+     * @param {String} propertyFrom Name of the property to pick the data from
+     * @param {String} propertyTo Name of the property to store the data on
+     * @param {Function} transistorFunc Function to be applied on the value of the property
+     * @returns {Observable} An observable sequence with objects that carry an (possibly) new property with a value coming from a (possibly) different property
+     */
     observableProto.convertProperty = function (propertyFrom, propertyTo, transistorFunc) {
         return this.select(function (x) {
             if (x.hasOwnProperty(propertyFrom) && isFunction(transistorFunc)) {
@@ -89,11 +94,12 @@
         });
     };
 
-    /// <summary>
-    /// Projects the value of a property on the current object into the new output of the stream
-    /// </summary>
-    /// <param name="propertyName">Name of the property to pick the data from</param>
-    /// <returns>An observable sequence with objects that where previously found on an objects property</returns>
+    /**
+     * Projects the value of a property on the current object into the new output of the stream
+     *
+     * @param {String} propertyName Name of the property to pick the data from
+     * @returns {Observable} An observable sequence with objects that where previously found on an objects property
+     */
     observableProto.selectProperty = function (propertyName) {
         return this.select(function (x) {
             if (x.hasOwnProperty(propertyName)) {
@@ -104,22 +110,24 @@
         });
     };
 
-    /// <summary>
-    /// Drops the current value and returns the value of the given parameter instead
-    /// </summary>
-    /// <param name="as">value for the projection</param>
-    /// <returns>An observable sequence that carries the given value</returns>
+    /**
+     * Drops the current value and returns the value of the given parameter instead
+     *
+     * @param as value for the projection
+     * @returns {Observable} An observable sequence that carries the given value
+     */
     observableProto.selectAs = function (as) {
         return this.select(function (_) {
             return as;
         });
     };
 
-    /// <summary>
-    /// Takes an array of observable sequences and produces a value with the results of all given sequences as a single result
-    /// </summary>
-    /// <param name="sources">Either an array of observable sequences or multiple observable sequences as multiple parameters</param>
-    /// <returns>An observable sequence that carries an array wich the results of all observable sequences</returns>
+    /**
+     * Takes an array of observable sequences and produces a value with the results of all given sequences as a single result
+     *
+     * @param sources Either an array of observable sequences or multiple observable sequences as multiple parameters
+     * @returns {Observable} An observable sequence that carries an array wich the results of all observable sequences
+     */
     Observable.forkJoin = function (sources) {
 
         var tempSources = arguments.length > 1 ? arguments : sources;
@@ -141,12 +149,14 @@
                  .select(function (results) { return results.array; });
     };
 
-    /// <summary>
-    /// Combines two observable sequences. Notifies whenever the left sequence produces a value and applies a function that combines the values of the left sequence with the last value of the right sequence
-    /// </summary>
-    /// <param name="rightSource">Other observable sequence to combine with</param>
-    /// <param name="selector">Function to be applied on both values that produces the output for the new observable sequence</param>
-    /// <returns>An observable sequence that carries the result of the selector function of both combined streams</returns>
+    /**
+     * Combines two observable sequences. Notifies whenever the left sequence produces a value and applies a function that
+     * combines the values of the left sequence with the last value of the right sequence
+     *
+     * @param {Observable} rightSource Other observable sequence to combine with
+     * @param {Function} selector Function to be applied on both values that produces the output for the new observable sequence
+     * @returns {Observable} An observable sequence that carries the result of the selector function of both combined streams
+     */
     observableProto.combineLatestOnLeft = function (rightSource, selector) {
 
         return this.timestamp()
